f8ed2c42b698f5ff75db517b7fb25afd318a2685
AbileneM <abileneahue@gmail.com>
2026-04-14 00:24:36 -0400

    Modifier le fichier db.js

--- config/db.js
+++ config/db.js
@@ -2,15 +2,11 @@ import { Sequelize } from "sequelize"
 import dotenv from 'dotenv' //Importer les variables d'environnement
 
 const ENV = dotenv.config().parsed //Récupérer les variables d'environnement
+console.log(ENV)
 
-const connection = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-        host: process.env.DB_HOST,
-        dialect: ENV.DB_DIALECT
-    }
-);
+const connection = new Sequelize(ENV.DB_NAME, ENV.DB_USER, ENV.DB_PASSWORD, {
+    host: ENV.DB_HOST,
+    dialect: ENV.DB_DIALECT
+});
 
 export default connection;
